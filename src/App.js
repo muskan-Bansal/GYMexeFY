8ee4b16a0461ec73ac99070cf4ffac783550ce6f
Muskan-Bansal <105799897+muskan-Bansal@users.noreply.github.com>
2022-12-11 19:10:33 +0530

    Gym stuffs
    Gym stuffs

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import {Route,Routes,Switch} from 'react-router-dom';
+import {Box} from '@mui/material'; 
+import './App.css';
+
+import ExerciseDetail from './pages/ExerciseDetail';
+import Home from './pages/Home';
+ 
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Details from './components/Details';
+
+
+function App() {
+  return (
+   <> 
+    <Box width="400px" sx={{width:{xl:'1488px'}}} m="auto"> 
+        <Navbar/> 
+        <Routes>
+            <Route path="/"   element={<Home />}/> 
+           {/* <Switch>
+                <Route path="/exercises" element={<ExerciseDetail />}/> 
+                <Route path="/exercises/:id" element={<ExerciseDetail />}/> 
+           </Switch> */}
+              {/* <Route exact path="/exercises" element={<ExerciseDetail />}/>  */}
+              <Route    path="/exercise/:id" element={<ExerciseDetail />}/> 
+        </Routes>
+        <Footer/>
+
+        
+    </Box></>
+  );
+}
+
+export default App;
